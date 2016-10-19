e70e10245f468af46cf596ffe9c462015904de4b
Gobin Sougrakpam <gobinsougrakpam@gmail.com>
2016-10-19 18:58:27 +1100

    Adding unit tests for start/stop/terminate/status calls for AWS instances

---
+++ awsapi/instance.go
@@ -0,0 +1,9 @@
+package awsapi
+
+import (
+    "github.com/aws/aws-sdk-go/service/ec2"
+)
+
+func getInstanceStatus() {
+
+}
---
+++ awsapi/instance_test.go
@@ -0,0 +1,21 @@
+package awsapi
+
+import (
+    "testing"
+)
+
+func TestGetInstanceStatus(t *testing.T) {
+
+}
+
+func TestStartInstance(t *testing.T) {
+
+}
+
+func TestStopInstance(t *testing.T) {
+
+}
+
+func TestTerminateInstance(t *testing.T) {
+
+}
