20adcd04cfbff92156e6918590742cf3ca9a54b8
Gobin Sougrakpam <gobinsougrakpam@gmail.com>
2016-10-30 09:58:10 +1100

    Adding unit test for getInstanceStatus

--- awsapi/instance.go
+++ awsapi/instance.go
@@ -1,9 +1,21 @@
 package awsapi
 
 import (
+    "github.com/aws/aws-sdk-go/aws"
     "github.com/aws/aws-sdk-go/service/ec2"
 )
 
-func getInstanceStatus(ec2client *ec2.EC2) {
-    ec2client.DescribeInstanceStatus(input)
+type client interface {
+    DescribeInstanceStatus(input *ec2.DescribeInstanceStatusInput) (*ec2.DescribeInstanceStatusOutput, error)
+}
+
+func getInstanceStatus(c client, instanceID string) (string, error) {
+    input := &ec2.DescribeInstanceStatusInput{
+        InstanceIds: []*string{
+            aws.String(instanceID),
+        },
+    }
+    output, err := c.DescribeInstanceStatus(input)
+    status := output.InstanceStatuses[0]
+    return status.InstanceState.String(), err
 }
--- awsapi/instance_test.go
+++ awsapi/instance_test.go
@@ -1,11 +1,39 @@
 package awsapi
 
 import (
+    "github.com/aws/aws-sdk-go/aws"
+    ec2 "github.com/aws/aws-sdk-go/service/ec2"
     "testing"
 )
 
-func TestGetInstanceStatus(t *testing.T) {
+type testclient struct {
+}
 
+func (t testclient) DescribeInstanceStatus(input *ec2.DescribeInstanceStatusInput) (*ec2.DescribeInstanceStatusOutput, error) {
+    instancesIDs := input.InstanceIds
+    state := &ec2.InstanceState{
+        Code: aws.Int64(16),
+        Name: aws.String("running"),
+    }
+
+    status := &ec2.InstanceStatus{
+        AvailabilityZone: aws.String("Test Zone"),
+        InstanceId:       instancesIDs[0],
+        InstanceState:    state,
+    }
+
+    ouput := &ec2.DescribeInstanceStatusOutput{
+        InstanceStatuses: []*ec2.InstanceStatus{status},
+    }
+    return ouput, nil
+}
+func TestGetInstanceStatus(t *testing.T) {
+    tc := testclient{}
+    _, err := getInstanceStatus(tc, "i-testinstance")
+
+    if err != nil {
+        t.Error("Error calling getInstanceStatus")
+    }
 }
 
 func TestStartInstance(t *testing.T) {
